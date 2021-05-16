475b293f4b665197bdbeb05396f0625c9b8d38bb
emaxximov <maksimov.20.43@gmail.com>
2021-05-16 13:44:15 +0300

    initial

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import telebot
 from telebot import types
 import uuid
 from boto.s3.connection import S3Connection
+import os
 
 # данные телеграма
 token = S3Connection(os.environ['api_token'])
+# with open('config.json', 'r') as config_file:
+#   config = json.load(config_file)
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
