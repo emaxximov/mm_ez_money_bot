c3f58e5d9d0d681ccb28e7372ccd51a76fb886a0
emaxximov <maksimov.20.43@gmail.com>
2021-05-22 19:00:01 +0300

    upd pas

--- pas.py
+++ pas.py
@@ -1,9 +1,12 @@
 import json
+import match
+import copy
 
 class pas(object):
     def __init__(self,Id):
         self.id = Id
         self.d = []
+        self.scores = copy.deepcopy(match.scores)
 
     def distinct(self,name):
         for i in self.d:
@@ -11,11 +14,20 @@ class pas(object):
                 return False
         return True
 
-    def add(self,name,score):
-        if self.distinct(name):
-            self.d.append((name,score))
-            return True
-        return False
+    def add(self,name,path):
+        if not self.distinct(name):
+            return False
+        score = 0
+        score_list = self.scores
+        for i in path:
+            score_list = score_list[i]
+        for i in range(len(score_list)):
+            if score_list[i] > 0:
+                score = score_list[i]
+            score_list[i] = 0
+            break
+        self.d.append((name,score,path))
+        return True
 
     def get_list(self):
         s = ''
@@ -27,11 +39,16 @@ class pas(object):
         for i in range(len(self.d)):
             if self.d[i][0] == name:
                 del self.d[i]
+                np = self.d[:]
+                self.delete_all()
+                for i in np:
+                    self.add(i[0],i[2])
                 return True
         return False
 
     def delete_all(self):
         self.d = []
+        self.scores = copy.deepcopy(match.scores)
 
     def total(self):
         return sum([i[1] for i in self.d])
@@ -63,3 +80,6 @@ class db_obj(object):
             s[pas.id] = pas.d
         with open('db.json', 'w') as write_file:
             json.dump(s,write_file,indent=2)
+
+# p = pas(228)
+# p.add('kek',)
